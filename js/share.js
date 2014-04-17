6e5359865b31826659a238a61bc228aec39800e0
josefmonje <josefmonje@gmail.com>
2014-04-17 13:51:50 +0800

    Map of UP Diliman
    This is a fast-loading map of UP Diliman. Created using officially
    published information and Google Static Maps API.

---
+++ js/share.js
@@ -0,0 +1,12 @@
+var share = new Share(".share", {
+ui: {
+    flyout: "bottom right",
+    button_background: "white",
+    button_color: "black",
+},
+networks: {
+    facebook: {
+      app_id: "1423726704545993",
+    }
+  }
+});
